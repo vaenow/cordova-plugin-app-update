a3230a99bf4a103e47beef20402e4dfaa94f2fa3
vaenow <luowenbiz@gmail.com>
2016-02-03 19:35:26 +0800

    update npm

--- www/AppUpdate.js
+++ www/AppUpdate.js
@@ -1,23 +1,6 @@
 var exec = require('cordova/exec');
 
-var _defaultUIValues = {
-    updateLatest       : "",
-    updateTitle        : "",
-    updateMessage      : "",
-    updateUpdateBtn    : "",
-    updating           : "",
-    updateCancel       : "",
-    updateErrorTitle   : "",
-    updateErrorMessage : "",
-    updateErrorYesBtn  : "",
-};
-
-exports.checkAppUpdate = function(success, error, updateUrl, UIValues) {
-    UIValues = UIValues ? [UIValues] : [];
-    Object.keys(_defaultUIValues).forEach(function(k) {
-        UIValues[k] = UIValues[k] || _defaultUIValues[k];
-    });
-    updateUrl && UIValues.push(updateUrl);
-	var updateArgs = UIValues;
-    exec(success, error, "AppUpdate", "checkAppUpdate", updateArgs);
+exports.checkAppUpdate = function(success, error, updateUrl) {
+    updateUrl = updateUrl ? [updateUrl] : [];
+    exec(success, error, "AppUpdate", "checkAppUpdate", updateUrl);
 };
