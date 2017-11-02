768ed9aa20de9c8e14412f0ce00f01392ef9336a
Wen Luo <vaenow@users.noreply.github.com>
2017-11-02 09:56:01 -0500

    fix issue #64
    fix issue #64

--- www/AppUpdate.js
+++ www/AppUpdate.js
@@ -3,5 +3,6 @@ var exec = require('cordova/exec');
 exports.checkAppUpdate = function(success, error, updateUrl, options) {
     updateUrl = updateUrl ? updateUrl : '';
     options = options ? options : {};
+    options.authType = options.authType || ""; // fix issue #64
     exec(success, error, "AppUpdate", "checkAppUpdate",  [updateUrl, options]);
 };
